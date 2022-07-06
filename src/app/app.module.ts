34246725473cea90ebf2e4b33619e66a65c98892
Milo <jerezemilio@hotmail.com>
2022-07-06 20:18:54 -0300

    vamos mal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './componentes/principal/principal.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent,
   ],
   imports: [
     BrowserModule,
