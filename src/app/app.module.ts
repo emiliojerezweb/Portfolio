ed0e3c0d9c05f7880a18aa4d707aac1bd56957b4
Milo <jerezemilio@hotmail.com>
2022-07-11 19:00:43 -0300

    conexiones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrincipalComponent } from './componentes/principal/principal.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { SobreMiComponent } from './componentes/sobre-mi/sobre-mi.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EstudiosComponent } from './componentes/estudios/estudios.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PrincipalComponent,
+    MenuComponent,
+    SobreMiComponent,
+    ExperienciaComponent,
+    EstudiosComponent,
   ],
   imports: [
     BrowserModule,
