e88a1deb2500e5d8d9a2bc159376af192d6c46e7
Milo <jerezemilio@hotmail.com>
2022-07-15 13:01:05 -0300

    Configurar contacto-curso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { CursosComponent } from './componentes/cursos/cursos.component';
 import { EstudiosComponent } from './componentes/estudios/estudios.component';
 import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
 import { PrincipalComponent } from './componentes/principal/principal.component';
@@ -9,9 +11,10 @@ const routes: Routes = [
   {path:'', component: PrincipalComponent},
   {path:'sobremi', component: SobreMiComponent},
   {path:'estudios', component: EstudiosComponent},
-  {path:'experiencia', component: ExperienciaComponent}
+  {path:'experiencia', component: ExperienciaComponent},
+  {path:'curso', component: CursosComponent},
+  {path:'contacto', component:ContactoComponent}
   
-
 ];
 
 @NgModule({
